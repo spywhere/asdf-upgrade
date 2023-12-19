66ffe8976df48b6a6bbd62221387117d8c7a095b
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-12-19 15:03:41 +0700

    refactor: use a single binary

--- lib/commands/command-global.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../upgrade.sh"
-
-upgrade_package global "$@"
--- lib/commands/command.bash
+++ lib/commands/command.bash
@@ -2,4 +2,13 @@
 
 set -eo pipefail
 
-echo "usage: asdf upgrade <scope> <package>"
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../upgrade.sh"
+
+if [[ $# -lt 2 ]]; then
+  echo "usage: asdf upgrade global <plugin>"
+  echo "       asdf upgrade local <plugin>"
+  echo "       asdf upgrade shell <plugin>"
+  exit 1
+fi
+
+upgrade_package "$1" "$2"
